3fbac440534a264a23ca1322889a30995d9bd2ea
Joe Savage <joe.savage@gmail.com>
2021-05-16 16:58:13 -0500

    updated sponsors

--- src/views/Sponsors.vue
+++ src/views/Sponsors.vue
@@ -22,15 +22,15 @@
                             :key="index"
                         >
                             <v-card :href="sponsor.link" target="_blank">
-                                <v-img
+                                <v-img :contain="sponsor.containImage"
                                     :src="sponsor.image"
                                     class="white--text align-end"
                                     gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
                                     height="200px"
                                 >
                                     <v-card-title v-text="sponsor.name">
-                                        
                                     </v-card-title>
+                                    <v-card-subtitle class="white--text text-left" v-text="sponsor.subtitle" />
                                 </v-img>
                             </v-card>
                         </v-col>
@@ -59,34 +59,57 @@ export default {
         sponsors: [
             {
                 name: "Nicole Beailieu",
+                subtitle: "",
                 image: require("@/assets/nicole_bjpg.jpg"),
+                containImage: false
             },
             {
-                name: "The Bealle Family",
-                image: require("@/assets/gary_guynes.gif"),
+                name: "Mr. B Tutoring",
+                subtitle: "512-971-1141 rhois.002378@sbcglobal.net",
+                image: require("@/assets/mr_b_tutoring.png"),
+                link: "mailto:rhois.002378@sbcglobal.net",
+                containImage: true
             },
             {
                 name: "Marc Digby",
+                subtitle: "",
                 image: require("@/assets/marc_digby.jpg"),
+                containImage: false
             },
             {
                 name: "Gary Guynes",
+                subtitle: "",
                 image: require("@/assets/gary_guynes.gif"),
+                containImage: true
             },
             {
                 name: "Dale McPherson",
+                subtitle: "",
                 image: require("@/assets/dale_mcpherson.jpg"),
+                containImage: false
             },
             {
                 name: "Mungia Real Estate",
+                subtitle: "mungiarealestate.com",
                 image: require("@/assets/mungia_real_estate.jpg"),
                 link: "https://www.mungiarealestate.com/",
+                containImage: false
             },
             {
                 name:
-                    "Cedar Park High School PTSA",
+                    "Cedar Park HS PTSA",
+                    subtitle: "cphs.my-pta.org",
                 image: require("@/assets/ptsa_logo.jpg"),
-                link: "http://cphs.my-pta.org/"
+                link: "http://cphs.my-pta.org/",
+                containImage: true
+            },
+            {
+                name:
+                    "Rudy's Country Store & BBQ",
+                    subtitle: "rudysbbq.com",
+                image: require("@/assets/rudys.png"),
+                link: "https://rudysbbq.com/",
+                containImage: true
             },
         ],
     }),
